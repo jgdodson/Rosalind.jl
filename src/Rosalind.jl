9343f771198a2d3bb63b82ee949ae2cd5fe0aa3d
Jordan Dodson <jordandodson22@gmail.com>
2015-05-25 01:07:59 -0500

    adding GenSeq, DNASeq, RNASeq

--- src/Rosalind.jl
+++ src/Rosalind.jl
@@ -1,66 +1,117 @@
 module Rosalind
 
+# (✓) Create abstract type for genetic sequence
+# ( ) Add concrete protein sequence type
+
+abstract GenSeq
+
+
+immutable DNASeq <: GenSeq
+  seq::String
+
+  DNASeq(seq) = isDNAString(seq) ? new(seq) : error("String contains non-ACTG character.")
+end
+
+immutable RNASeq <: GenSeq
+  seq::String
+
+  RNASeq(seq) = isRNAString(seq) ? new(seq) : error("String contains non-ACGU character.")
+end
+
+function isDNAString(seq::String)
+
+  bases = ['A','C','G','T']
+
+  all(b -> b in bases, seq)
+end
+
+function isRNAString(seq::String)
+
+  bases = ['A','C','G','U']
+
+  all(b -> b in bases, seq)
+end
+
+
 # Frequency counts for the bases in a DNA sequence.
-function countBasesDNA(seq :: String)
-  
+function countBasesDNA(dna::DNASeq)
+
   counts::Array{Int64, 1} = zeros(4)
-  
-  for c in seq
+
+  for c in dna.seq
     if c == 'A'
       counts[1] += 1
     elseif c == 'C'
       counts[2] += 1
     elseif c == 'G'
       counts[3] += 1
-    elseif c == 'T' 
+    else
       counts[4] += 1
-    else error("DNA sequence contains non-ACGT character")
     end
   end
-  
+
   counts
 end
 
-function transcribe(dna::String)
-  replace(dna, "T", "U")
+# Related problem - http://rosalind.info/problems/rna
+function transcribe(dna::DNASeq)
+  RNASeq(replace(dna.seq, "T", "U"))
 end
 
-function complementDNA(dna :: String)
+# Related problem - http://rosalind.info/problems/revc
+function complement(dna::DNASeq)
 
-  map((ch) ->
-    if ch == 'A' 
-      'T'
-    elseif ch == 'C' 
-      'G'
-    elseif ch == 'G' 
-      'C'
-    elseif ch == 'T' 
-      'A'
-    else
-      error("DNA sequence contains non-ACTG character")
-    end, dna)
+  complements = ['A'=>'T',
+                 'C'=>'G',
+                 'G'=>'C',
+                 'T'=>'A']
+
+  DNASeq(map((ch) -> complements[ch], dna.seq))
 end
 
-function reverseComplementDNA(dna :: String)
-  reverse(complementDNA(dna))
+function complement(rna::RNASeq)
+
+  complements = ['A'=>'U',
+                 'C'=>'G',
+                 'G'=>'C',
+                 'U'=>'A']
+
+  RNASeq(map((ch) -> complements[ch], rna.seq))
 end
 
-function countMutations(seq1::String, seq2::String)
+function length(seq::GenSeq)
+  Base.length(seq.seq)
+end
+
+# Related problem - http://rosalind.info/problems/revc
+function reverse(dna::DNASeq)
+  DNASeq(reverse(dna.seq))
+end
+
+function reverse(rna::RNASeq)
+  RNASeq(reverse(rna.seq))
+end
+
+# Related problem - http://rosalind.info/problems/hamm
+function hammingDistance(seq1::GenSeq, seq2::GenSeq)
+
+  seq1Len = length(seq1)
+  seq2Len = length(seq2)
 
   # ensure sequence lengths are equal
-  length(seq1) == length(seq2) || 
-  error("sequences must have same length")
+  seq1Len == seq2Len ||
+  error("Sequences must have same length.")
 
   # count mismatches of corresponding bases
-  foldl((acc, next) ->
-    if seq1[next] != seq2[next]
+  foldl((acc, i) ->
+    if seq1.seq[i] != seq2.seq[i]
       acc + 1
     else
       acc
-    end, 0, 1:length(seq1))
+    end, 0, 1:seq1Len)
 end
 
-function findMotif(seq :: String, motif :: String)
+function findMotif(seq::String, motif::String)
 
   seqLen = length(seq)
   motifLen = length(motif)
@@ -76,12 +127,14 @@ function findMotif(seq :: String, motif :: String)
 
 end
 
-function loadSeq(filename :: String)
-  
-  # open datafile
+function loadDNASeq(filename :: String)
+
+  # read text from file
   raw = open(readall, filename)
-  
-  replace(strip(raw), r"\n|\r", "")
+
+  clean = replace(strip(raw), r"\n|\r", "")
+
+  DNASeq(clean)
 end
 
 function linesFromFile(filename::String)
